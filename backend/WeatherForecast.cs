2a3d7e9fb9c6e15058a264b31afca69ed7caf143
NORIX <tmanh15112003@gmail.com>
2023-09-29 17:20:56 +0700

    Login duoc khong?

--- backend/WeatherForecast.cs
+++ backend/WeatherForecast.cs
@@ -6,6 +6,10 @@ public class WeatherForecast
 
     public int TemperatureC { get; set; }
 
+    public int Uptime { get; set; }
+    
+
+
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
